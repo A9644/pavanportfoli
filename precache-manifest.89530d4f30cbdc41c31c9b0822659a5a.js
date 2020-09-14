7e1fb21e45201f5f567f1c9898a3fa6f354a96c5
AnandSharma <anand9644.sharma@gmail.com>
2020-09-15 00:47:53 +0530

    Updates

---
+++ precache-manifest.89530d4f30cbdc41c31c9b0822659a5a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc88f2113007ac0fb3c74cd62a35701",
+    "url": "/pavanportfoli/index.html"
+  },
+  {
+    "revision": "4e48466a5ce9ef32d0e2",
+    "url": "/pavanportfoli/static/css/2.df750b2b.chunk.css"
+  },
+  {
+    "revision": "69c957cff0bc242d39f4",
+    "url": "/pavanportfoli/static/css/main.5b020eae.chunk.css"
+  },
+  {
+    "revision": "4e48466a5ce9ef32d0e2",
+    "url": "/pavanportfoli/static/js/2.6ba9ab30.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/pavanportfoli/static/js/2.6ba9ab30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c957cff0bc242d39f4",
+    "url": "/pavanportfoli/static/js/main.729140dc.chunk.js"
+  },
+  {
+    "revision": "3455a103cf5711606f3c",
+    "url": "/pavanportfoli/static/js/runtime-main.79296e1c.js"
+  },
+  {
+    "revision": "fd100431d7ce2bace6342bc803781623",
+    "url": "/pavanportfoli/static/media/Banner.fd100431.jpg"
+  },
+  {
+    "revision": "80aebfe6b4997150366f67572e5b091c",
+    "url": "/pavanportfoli/static/media/Logo.80aebfe6.png"
+  },
+  {
+    "revision": "0887890a7a2ad79c9b11f14257f77a46",
+    "url": "/pavanportfoli/static/media/NewBaner.0887890a.png"
+  },
+  {
+    "revision": "df671c9a624a1426bfebf9c1ab4e7a80",
+    "url": "/pavanportfoli/static/media/Pavannew.df671c9a.jpeg"
+  },
+  {
+    "revision": "5729804efd68320a1b56b84f333879d9",
+    "url": "/pavanportfoli/static/media/logos.5729804e.jpg"
+  },
+  {
+    "revision": "dc39ef28af812d549e437646eb916976",
+    "url": "/pavanportfoli/static/media/myimg.dc39ef28.jpg"
+  },
+  {
+    "revision": "2faac5cff947e0b76166355f4a98aae1",
+    "url": "/pavanportfoli/static/media/newBrochure.2faac5cf.jpg"
+  },
+  {
+    "revision": "1672c9a692de796848d6b2be557a8678",
+    "url": "/pavanportfoli/static/media/newpam.1672c9a6.jpg"
+  },
+  {
+    "revision": "486230bd72c243c7ea976fff4607d3e0",
+    "url": "/pavanportfoli/static/media/social.486230bd.png"
+  },
+  {
+    "revision": "010db248966e42ed4bde63bf13b06820",
+    "url": "/pavanportfoli/static/media/visitingcards.010db248.jpg"
+  }
+]);
